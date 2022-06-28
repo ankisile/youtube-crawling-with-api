b51cd3c422dbd152da22a175ac99d9d14dc77bd0
siss-ankisile <ankisile@gmail.com>
2022-06-28 15:23:14 +0900

    url parse

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ import pandas as pd #엑셀 형태로 저장하기 위한 라이브러리
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 import json
+from urllib.parse import urlparse, parse_qs
 from pprint import pprint
 
 #service => connetion to youtube data api
@@ -35,7 +36,7 @@ def get_video(service, video_id):
 # SCOPES = ["https://www.googleapis.com/auth/youtube"]
 # API_NAME = "youtube"
 # API_VERSION = "v3"    
-def main():
+def main(video_id):
     DEVELOPER_KEY = "AIzaSyDqMOpJralVVgKQSRsnpHrl6L6v2Qatzjc"
     YOUTUBE_API_SERVICE_NAME = "youtube"
     YOUTUBE_API_VERSION = "v3"
@@ -43,7 +44,6 @@ def main():
     service = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
         developerKey=DEVELOPER_KEY)
 
-    video_id = "n1Fs9wxw8ro"
 
     video = get_video(service, video_id)
 
@@ -67,15 +67,42 @@ def main():
 
         row.append([video_url, video_title, publish_date, channel_name, view_count, comment_count, like_count, video_desc, thumbnail])
         df = pd.DataFrame(data=row, columns=columns)
+        
 
         if os.path.isfile("youtube.xlsx"):
-            df2 = pd.read_excel("youtube.xlsx")
-            df2.append(df)
+            df2 = pd.read_excel("youtube.xlsx",index_col = 0)
+            df= pd.concat([df2,df], axis=0)
+           
 
         df.to_excel("youtube.xlsx")
+        print(df)
 
 
 
+# https://stackoverflow.com/questions/4356538/how-can-i-extract-video-id-from-youtubes-link-in-python
+def video_id(value):
+    """
+    Examples:
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(value)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    # fail?
+    return None
 
-if __name__ == "__main__":
-    main() 
+
+if __name__ == "__main__":    
+    video_id = video_id("https://www.youtube.com/watch?v=ZboVgFP3V2g")
+    main(video_id) 
